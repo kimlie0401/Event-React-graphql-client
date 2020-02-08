682080d0ce188d31694646cb2dd807ff02d9ac26
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-02-08 10:48:35 -0800

    Final!

---
+++ build/precache-manifest.470b260b32f425667695cf3e0005c212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dde9f5d1d31b76ef1a8ab42f650a100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d05a5cc3e303672cbf45",
+    "url": "/static/css/main.321cfa73.chunk.css"
+  },
+  {
+    "revision": "6403f48750cef373a089",
+    "url": "/static/js/2.d4b7f51e.chunk.js"
+  },
+  {
+    "revision": "2bc439fdf8f7fbd6d73ed1defeccdb76",
+    "url": "/static/js/2.d4b7f51e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d05a5cc3e303672cbf45",
+    "url": "/static/js/main.dd5aad0a.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
